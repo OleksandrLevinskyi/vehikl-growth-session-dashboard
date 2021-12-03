87162555a55b6c2f4342feca0746e68d2f3152fc
Vehikl Team <go@vehikl.com>
2021-12-03 16:55:49 -0500

    wip

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
 
 describe("Growth Sessions Dashboard App", () => {
   test('renders the heading', () => {
@@ -13,6 +14,26 @@ describe("Growth Sessions Dashboard App", () => {
     render(<App/>);
     expect(screen.getByText(/All Time/)).toBeInTheDocument();
     expect(screen.getByText(/Monthly/)).toBeInTheDocument();
+  });
+
+  test('renders all time view when all time option is selected', async () => {
+    render(<App/>);
+
+    const link = screen.getByText(/All Time/);
+    await userEvent.click(link);
+
+    expect(screen.getByText(/all time option is selected/)).toBeInTheDocument();
+    expect(screen.getByText(/monthly option is selected/)).not.toBeInTheDocument();
+  });
+
+  test('renders all time view when all time option is selected', async () => {
+    render(<App/>);
+
+    const link = screen.getByText(/Monthly/);
+    await userEvent.click(link);
+
+    expect(screen.getByText(/all time option is selected/)).not.toBeInTheDocument();
+    expect(screen.getByText(/monthly option is selected/)).toBeInTheDocument();
   });
 
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Button, Flex, Heading, Spacer} from "@chakra-ui/react";
 
 function App() {
-  return (
-      <>
-          <Flex>
-              <Heading>GS Analytics Board</Heading>
-              <Spacer/>
-              <Button>All Time</Button>
-              <Button>Monthly</Button>
-          </Flex>
+    const [text, setText] = useState(() => "");
 
-      </>
+    function selectAllTime() {
+        console.log('')
+        setText("all time option is selected")
+    }
+
+    function selectMonthly() {
+        setText("monthly option is selected")
+    }
+
+    return (
+        <>
+            <Flex>
+                <Heading>GS Analytics Board</Heading>
+                <Spacer/>
+                <Button onClick={selectAllTime}>All Time</Button>
+                <Button onClick={selectMonthly}>Monthly</Button>
+            </Flex>
+            {text}
+        </>
   );
 }
 
