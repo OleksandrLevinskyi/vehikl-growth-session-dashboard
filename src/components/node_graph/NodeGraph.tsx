83d3b851a6ea0d3392ff1eee850c1c2de36a7e15
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2021-12-17 16:43:14 -0500

    wip

--- src/components/node_graph/NodeGraph.tsx
+++ src/components/node_graph/NodeGraph.tsx
@@ -4,7 +4,7 @@ import {useEffect, useState} from "react";
 import {Button} from "@chakra-ui/react";
 
 function NodeGraph() {
-    const [data, setData] = useState();
+    const [data, setData] = useState<any>();
 
     useEffect(() => {
         fetch('http://127.0.0.1:8000/nodegraph')
@@ -20,100 +20,113 @@ function NodeGraph() {
             )
     }, []);
 
-    var svg = d3.select("svg"),
-        width = +svg.attr("width"),
-        height = +svg.attr("height");
+    function loadNodeGraph() {
+        let svg = d3.select("svg"),
+            width = +svg.attr("width"),
+            height = +svg.attr("height");
 
-    // var color = d3.scaleOrdinal(d3.schemeCategory20);
-    //
-    // var simulation = d3.forceSimulation()
-    //     .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    //     .force("charge", d3.forceManyBody())
-    //     .force("center", d3.forceCenter(width / 2, height / 2));
-    //
-    // d3.json("miserables.json", function(error, graph) {
-    //     if (error) throw error;
-    //
-    //     var link = svg.append("g")
-    //         .attr("class", "links")
-    //         .selectAll("line")
-    //         .data(graph.links)
-    //         .enter().append("line")
-    //         .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
-    //
-    //     var node = svg.append("g")
-    //         .attr("class", "nodes")
-    //         .selectAll("g")
-    //         .data(graph.nodes)
-    //         .enter().append("g")
-    //
-    //     var circles = node.append("circle")
-    //         .attr("r", 5)
-    //         .attr("fill", function(d) { return color(d.group); });
-    //
-    //     // Create a drag handler and append it to the node object instead
-    //     var drag_handler = d3.drag()
-    //         .on("start", dragstarted)
-    //         .on("drag", dragged)
-    //         .on("end", dragended);
-    //
-    //     drag_handler(node);
-    //
-    //     var lables = node.append("text")
-    //         .text(function(d) {
-    //             return d.id;
-    //         })
-    //         .attr('x', 6)
-    //         .attr('y', 3);
-    //
-    //     node.append("title")
-    //         .text(function(d) { return d.id; });
-    //
-    //     simulation
-    //         .nodes(graph.nodes)
-    //         .on("tick", ticked);
-    //
-    //     simulation.force("link")
-    //         .links(graph.links);
-    //
-    //     function ticked() {
-    //         link
-    //             .attr("x1", function(d) { return d.source.x; })
-    //             .attr("y1", function(d) { return d.source.y; })
-    //             .attr("x2", function(d) { return d.target.x; })
-    //             .attr("y2", function(d) { return d.target.y; });
-    //
-    //         node
-    //             .attr("transform", function(d) {
-    //                 return "translate(" + d.x + "," + d.y + ")";
-    //             })
-    //     }
-    // });
-    //
-    // function dragstarted(d) {
-    //     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    //     d.fx = d.x;
-    //     d.fy = d.y;
-    // }
-    //
-    // function dragged(d) {
-    //     d.fx = d3.event.x;
-    //     d.fy = d3.event.y;
-    // }
-    //
-    // function dragended(d) {
-    //     if (!d3.event.active) simulation.alphaTarget(0);
-    //     d.fx = null;
-    //     d.fy = null;
-    // }
+        var simulation = d3.forceSimulation()
+            .force("link", d3.forceLink([...data.edges]))
+            .force("charge", d3.forceManyBody())
+            .force("center", d3.forceCenter(width / 2, height / 2));
+
+        var link = svg.append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(data.edges)
+            .enter().append("line")
+            .attr("stroke-width", function (d: any) {
+                return Math.sqrt(d.value);
+            });
+
+        var node = svg.append("g")
+            .attr("class", "nodes")
+            .selectAll("g")
+            .data(data.nodes)
+            .enter()
+            .append("g")
+
+        var circles = node.append("circle")
+            .attr("r", 5)
+            .attr("fill", "red");
+
+        // Create a drag handler and append it to the node object instead
+        // var drag_handler = d3.drag()
+        //     .on("start", dragstarted)
+        //     .on("drag", dragged)
+        //     .on("end", dragended);
+        //
+        // drag_handler(node);
+
+        var lables = node.append("text")
+            .text(function (d: any) {
+                return d.name;
+            })
+            .attr('x', 6)
+            .attr('y', 3);
+
+        node.append("title")
+            .text(function (d: any) {
+                return d.id;
+            });
+
+        simulation
+            .nodes(data.nodes)
+            .on("tick", ticked);
+
+        simulation.force<any>("link")
+            .links(data.edges);
+
+        function ticked() {
+            link
+                .attr("x1", function (d: any) {
+                    return d.source.x;
+                })
+                .attr("y1", function (d: any) {
+                    return d.source.y;
+                })
+                .attr("x2", function (d: any) {
+                    return d.target.x;
+                })
+                .attr("y2", function (d: any) {
+                    return d.target.y;
+                });
+
+            node
+                .attr("transform", function (d: any) {
+                    return "translate(" + d.x + "," + d.y + ")";
+                })
+
+
+            function dragstarted(event: any, d: any) {
+                if (!event.active) simulation.alphaTarget(0.3).restart();
+                d.fx = d.x;
+                d.fy = d.y;
+            }
+
+            function dragged(event: any, d: any) {
+                d.fx = event.x;
+                d.fy = event.y;
+            }
+
+            function dragended(event: any, d: any) {
+                if (!event.active) simulation.alphaTarget(0);
+                d.fx = null;
+                d.fy = null;
+            }
+        }
+    }
+
 
     return (
         <>
-            <Button onClick={() => console.log(data)}>Get Data</Button>
-            <svg width="960" height="600"></svg>
-            <script src="https://d3js.org/d3.v4.min.js"></script>
+            <Button onClick={() => {
+                loadNodeGraph()
+            }}>Get Data + Create SVG</Button>
+            <svg width="960" height="600"/>
         </>
     );
 }
+
 
 export default NodeGraph;
