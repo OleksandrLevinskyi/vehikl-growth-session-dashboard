03b967b76eb38b607a419d73d6152d4e8dc10d36
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-01-10 16:44:59 -0500

    Add date parameters

--- src/components/grafana_dashboard/GrafanaDashboard.tsx
+++ src/components/grafana_dashboard/GrafanaDashboard.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './GrafanaDashboard.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function GrafanaDashboard() {
+    const [startDate, setStartDate] = useState(new Date('2020-05-21'));
+    const [endDate, setEndDate] = useState(new Date());
+
+    const onChange = (dates: any) => {
+        const [start, end] = dates;
+        setStartDate(start);
+        setEndDate(end)
+
+        console.log(start.getTime())
+    };
+
     return (
         <>
-            <iframe src="http://localhost:3005/d/IvJGVgTnk/new-dashboard?orgId=1&kiosk&theme=light" frameBorder="0"/>
+            <DatePicker
+                selected={startDate}
+                onChange={onChange}
+                startDate={startDate}
+                endDate={endDate}
+                selectsRange
+                inline
+            />
+            <iframe
+                src={`http://localhost:3005/d/ndxFSP07k/stats?orgId=1&var-from=${startDate.toISOString().split('T')[0]}&var-to=${(endDate ? endDate : startDate).toISOString().split('T')[0]}&from=${startDate.getTime()}&to=${endDate ? endDate.getTime() : 'now'}&theme=light`}
+                frameBorder="0"/>
         </>
     );
 }
