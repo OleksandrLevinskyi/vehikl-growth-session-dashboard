0c377e121d45e6d65f92fbd9e141f8e12e5f72b4
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-02-03 22:41:03 -0500

    wip: display heatmap

--- src/components/heat_map/HeatMap.tsx
+++ src/components/heat_map/HeatMap.tsx
@@ -44,8 +44,8 @@ const HeatMap: React.FC = () => {
 
     const loadNewHeatMap = (data: any) => {
         let margin = {top: 80, right: 25, bottom: 30, left: 40},
-            width = 450 - margin.left - margin.right,
-            height = 450 - margin.top - margin.bottom;
+            width = 1200 - margin.left - margin.right,
+            height = 1200 - margin.top - margin.bottom;
 
         let svg = d3.select("#heat-map")
             .append("svg")
@@ -56,98 +56,83 @@ const HeatMap: React.FC = () => {
 
         console.log(data)
 
-            // var rows = d3.map(data, function (d) {
-            //     return d.group;
-            // }).keys()
-            // var cols = d3.map(data, function (d) {
-            //     return d.variable;
-            // }).keys()
-//
-//             // Build X scales and axis:
-//             var x = d3.scaleBand()
-//                 .range([0, width])
-//                 .domain(myGroups)
-//                 .padding(0.05);
-//             svg.append("g")
-//                 .style("font-size", 15)
-//                 .attr("transform", "translate(0," + height + ")")
-//                 .call(d3.axisBottom(x).tickSize(0))
+        let rows = d3.map(data, (dataPoint: any) => dataPoint.source);
+        let cols = d3.map(data, (dataPoint: any) => dataPoint.target);
+
+
+        let xScale = d3.scaleBand()
+            .range([0, width])
+            .domain(rows)
+            .padding(0.05);
+
+        // svg.append("g")
+        //     .style("font-size", 15)
+        //     .attr("transform", "translate(0," + height + ")")
+        //     .call(d3.axisBottom(xScale).tickSize(0))
+        // .select(".domain").remove();
+
+        let yScale = d3.scaleBand()
+            .range([height, 0])
+            .domain(cols)
+            .padding(0.05);
+
+        // svg.append("g")
+        //     .style("font-size", 15)
+        //     .call(d3.axisLeft(yScale).tickSize(0))
 //                 .select(".domain").remove()
-//
-//             // Build Y scales and axis:
-//             var y = d3.scaleBand()
-//                 .range([height, 0])
-//                 .domain(myVars)
-//                 .padding(0.05);
-//             svg.append("g")
-//                 .style("font-size", 15)
-//                 .call(d3.axisLeft(y).tickSize(0))
-//                 .select(".domain").remove()
-//
-//             // Build color scale
-//             var myColor = d3.scaleSequential()
-//                 .interpolator(d3.interpolateInferno)
-//                 .domain([1, 100])
-//
-//             // create a tooltip
-//             var tooltip = d3.select("#my_dataviz")
-//                 .append("div")
-//                 .style("opacity", 0)
-//                 .attr("class", "tooltip")
-//                 .style("background-color", "white")
-//                 .style("border", "solid")
-//                 .style("border-width", "2px")
-//                 .style("border-radius", "5px")
-//                 .style("padding", "5px")
-//
-//             // Three function that change the tooltip when user hover / move / leave a cell
-//             var mouseover = function (d) {
-//                 tooltip
-//                     .style("opacity", 1)
-//                 d3.select(this)
-//                     .style("stroke", "black")
-//                     .style("opacity", 1)
-//             }
-//             var mousemove = function (d) {
-//                 tooltip
-//                     .html("The exact value of<br>this cell is: " + d.value)
-//                     .style("left", (d3.mouse(this)[0] + 70) + "px")
-//                     .style("top", (d3.mouse(this)[1]) + "px")
-//             }
-//             var mouseleave = function (d) {
-//                 tooltip
-//                     .style("opacity", 0)
-//                 d3.select(this)
-//                     .style("stroke", "none")
-//                     .style("opacity", 0.8)
-//             }
-//
-//             // add the squares
-//             svg.selectAll()
-//                 .data(data, function (d) {
-//                     return d.group + ':' + d.variable;
-//                 })
-//                 .enter()
-//                 .append("rect")
-//                 .attr("x", function (d) {
-//                     return x(d.group)
-//                 })
-//                 .attr("y", function (d) {
-//                     return y(d.variable)
-//                 })
-//                 .attr("rx", 4)
-//                 .attr("ry", 4)
-//                 .attr("width", x.bandwidth())
-//                 .attr("height", y.bandwidth())
-//                 .style("fill", function (d) {
-//                     return myColor(d.value)
-//                 })
-//                 .style("stroke-width", 4)
-//                 .style("stroke", "none")
-//                 .style("opacity", 0.8)
-//                 .on("mouseover", mouseover)
-//                 .on("mousemove", mousemove)
-//                 .on("mouseleave", mouseleave)
+
+        let colorRange = d3.scaleSequential()
+            .interpolator(d3.interpolateInferno)
+            .domain([1, 100])
+
+        // let tooltip = d3.select("#heat-map")
+        //     .append("div")
+        //     .style("opacity", 0)
+        //     .attr("class", "tooltip")
+        //     .style("background-color", "white")
+        //     .style("border", "solid")
+        //     .style("border-width", "2px")
+        //     .style("border-radius", "5px")
+        //     .style("padding", "5px")
+
+        // const mouseover = (d:any) =>{
+        //     tooltip
+        //         .style("opacity", 1)
+        //     d3.select(this)
+        //         .style("stroke", "black")
+        //         .style("opacity", 1)
+        // }
+        // const mousemove = (d:any) =>{
+        //     tooltip
+        //         .html("The exact value of<br>this cell is: " + d.value)
+        //         .style("left", (d3.mouse(this)[0] + 70) + "px")
+        //         .style("top", (d3.mouse(this)[1]) + "px")
+        // }
+        // const mouseleave = (d:any) =>{
+        //     tooltip
+        //         .style("opacity", 0)
+        //     d3.select(this)
+        //         .style("stroke", "none")
+        //         .style("opacity", 0.8)
+        // }
+
+        svg.selectAll()
+            .data(data, (dataPoint: any) => dataPoint.source + ':' + dataPoint.target)
+            .enter()
+            .append("rect")
+            .attr("x", (dataPoint: any) => xScale(dataPoint.source) as any)
+            .attr("y", (dataPoint: any) => yScale(dataPoint.target) as any)
+            .attr("rx", 4)
+            .attr("ry", 4)
+            .attr("width", xScale.bandwidth())
+            .attr("height", yScale.bandwidth())
+            .style("fill", (dataPoint: any) => colorRange(dataPoint.weight))
+            .style("stroke-width", 4)
+            .style("stroke", "none")
+            .style("opacity", 0.8)
+            // .on("mouseover", mouseover)
+            // .on("mousemove", mousemove)
+            // .on("mouseleave", mouseleave)
 //
 // // Add title to graph
 //         svg.append("text")
@@ -168,6 +153,7 @@ const HeatMap: React.FC = () => {
 //             .text("A short description of the take-away message of this chart.");
 
     }
+
 
     return (
         <>
