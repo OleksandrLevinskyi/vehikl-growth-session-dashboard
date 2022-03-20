4e21dcdde1c98433015938d2111baf38b3f62714
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-03-19 22:04:13 -0400

    finalize checkbox list

--- src/components/checkbox_list/CheckboxList.tsx
+++ src/components/checkbox_list/CheckboxList.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import './CheckboxList.css';
+import {Box, VStack} from "@chakra-ui/react";
 
-function CheckboxList({nodes, multipleNodeIdsToFilterBy, setMultipleNodeIdsToFilterBy}:any) {
+function CheckboxList({nodes, multipleNodeIdsToFilterBy, setMultipleNodeIdsToFilterBy}: any) {
 
     function generateCheckBoxes() {
         return nodes
-            .map((node: any, key: number) => <span key={`span-${key}`}>
-                        <input type="checkbox" id={node.id}
-                               name="multipe_node_filter"
-                               key={`checkobx-${key}`}
-                               onChange={() => {
-                                   if (multipleNodeIdsToFilterBy.includes(node.id)) {
-                                       setMultipleNodeIdsToFilterBy([...multipleNodeIdsToFilterBy].filter((nodeId) => nodeId !== node.id));
-                                   } else {
-                                       setMultipleNodeIdsToFilterBy([...multipleNodeIdsToFilterBy, node.id])
-                                   }
-                               }}
-                               checked={isCheckboxChecked(node.id)}/>
-                        <label htmlFor={node.id}
-                               key={`label-${key}`}>{node.name}</label>
-                        <br/>
-                    </span>)
+            .map((node: any, key: number) =>
+                <Box as='label' key={`box-${key}`}>
+                    <input type="checkbox" id={node.id}
+                           name="multiple_node_filter"
+                           key={`checkbox-${key}`}
+                           onChange={() => {
+                               if (multipleNodeIdsToFilterBy.includes(node.id)) {
+                                   setMultipleNodeIdsToFilterBy([...multipleNodeIdsToFilterBy].filter((nodeId) => nodeId !== node.id));
+                               } else {
+                                   setMultipleNodeIdsToFilterBy([...multipleNodeIdsToFilterBy, node.id])
+                               }
+                           }}
+                           checked={isCheckboxChecked(node.id)} hidden/>
+                    <Box
+                        cursor='pointer'
+                        borderWidth='1px'
+                        borderRadius='md'
+                        boxShadow='md'
+                        bg={isCheckboxChecked(node.id) ? 'red.600' : ''}
+                        px={5}
+                        py={3}
+                    >
+                        {node.name}
+                    </Box>
+                </Box>)
     }
 
     const isCheckboxChecked = (nodeId: number) => {
@@ -32,7 +42,9 @@ function CheckboxList({nodes, multipleNodeIdsToFilterBy, setMultipleNodeIdsToFil
 
     return (
         <>
-            {generateCheckBoxes()}
+            <VStack>
+                {generateCheckBoxes()}
+            </VStack>
         </>
     );
 }
