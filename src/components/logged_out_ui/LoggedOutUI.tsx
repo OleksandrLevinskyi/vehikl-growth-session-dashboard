d30a0ecacad7562272be4c8b9c28fbcf9946ff23
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-06-02 15:06:47 -0400

    show error message for users (outside the organization) trying to log in

--- src/components/logged_out_ui/LoggedOutUI.tsx
+++ src/components/logged_out_ui/LoggedOutUI.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
     Flex,
-    Heading, Image,
+    Heading,
     Text
 } from "@chakra-ui/react";
 import Login from "../login/Login";
+import Cookies from "js-cookie";
 
 const LoggedOutUI: React.FC = () => {
+
+    const [isInvalidUser, setIsInvalidUser] = useState(false);
+
+    useEffect(() => {
+        const path = window.location.pathname;
+
+        if (path === '/invalid-user' && !Cookies.get('hash')) {
+            setIsInvalidUser(true);
+        }
+    }, []);
 
     return (
         <>
@@ -16,6 +27,12 @@ const LoggedOutUI: React.FC = () => {
                 <Text color='gray.400'>Please log in to view the dashboard.</Text>
 
                 <Login/>
+
+                {
+                    isInvalidUser &&
+                    <Text fontSize="sm" color="red.400">You are not an organization member. Please contact the
+                        administrator to get access to the dashboard.</Text>
+                }
             </Flex>
         </>
     );
